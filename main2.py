7ad0d5f6c9e85af23fbe4e85f996a239dbfb8659
James Silva Garcia <jamessilva0070@gmail.com>
2023-06-12 09:35:14 -0400

    Testing if api in main2.py

--- main2.py
+++ main2.py
@@ -64,44 +64,45 @@ def get_api_key():
 
 openai_api_key = get_api_key()
 
-col1, col2 = st.columns(2)
-with col1:
-    option_tone = st.selectbox(
-        'Which tone would you like your email to have?',
-        ('Formal', 'Informal'))
-    
-with col2:
-    option_dialect = st.selectbox(
-        'Which English Dialect would you like?',
-        ('American', 'British'))
-
-def get_text():
-    input_text = st.text_area(label="Email Input", label_visibility='collapsed', placeholder="Your Email...", key="email_input")
-    return input_text
-
-email_input = get_text()
-
-if len(email_input.split(" ")) > 700:
-    st.write("Please enter a shorter email. The maximum length is 700 words.")
-    st.stop()
-
-def update_text_with_example():
-    print ("in updated")
-    st.session_state.email_input = "Sally I am starts work at yours monday from dave"
-
-st.button("*See An Example*", type='secondary', help="Click to see an example of the email you will be converting.", on_click=update_text_with_example)
-
-st.markdown("### Your Converted Email:")
-
-if email_input:
-    if not openai_api_key:
-        st.warning('Please insert OpenAI API Key. Instructions [here](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key)', icon="⚠️")
-        st.stop()
-
-    llm = load_LLM(openai_api_key=openai_api_key)
-
-    prompt_with_email = prompt.format(tone=option_tone, dialect=option_dialect, email=email_input)
-
-    formatted_email = llm(prompt_with_email)
-
-    st.write(formatted_email)
+if openai_api_key !=""
+  col1, col2 = st.columns(2)
+  with col1:
+      option_tone = st.selectbox(
+          'Which tone would you like your email to have?',
+          ('Formal', 'Informal'))
+      
+  with col2:
+      option_dialect = st.selectbox(
+          'Which English Dialect would you like?',
+          ('American', 'British'))
+  
+  def get_text():
+      input_text = st.text_area(label="Email Input", label_visibility='collapsed', placeholder="Your Email...", key="email_input")
+      return input_text
+  
+  email_input = get_text()
+  
+  if len(email_input.split(" ")) > 700:
+      st.write("Please enter a shorter email. The maximum length is 700 words.")
+      st.stop()
+  
+  def update_text_with_example():
+      print ("in updated")
+      st.session_state.email_input = "Sally I am starts work at yours monday from dave"
+  
+  st.button("*See An Example*", type='secondary', help="Click to see an example of the email you will be converting.", on_click=update_text_with_example)
+  
+  st.markdown("### Your Converted Email:")
+  
+  if email_input:
+      if not openai_api_key:
+          st.warning('Please insert OpenAI API Key. Instructions [here](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key)', icon="⚠️")
+          st.stop()
+  
+      llm = load_LLM(openai_api_key=openai_api_key)
+  
+      prompt_with_email = prompt.format(tone=option_tone, dialect=option_dialect, email=email_input)
+  
+      formatted_email = llm(prompt_with_email)
+  
+      st.write(formatted_email)
